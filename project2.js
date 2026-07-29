0cbef2a33285982172d263298c0b52bd1b43d4bc
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-29 11:48:37 +0530

    calculatoe proejct is created and totally working

---
+++ project2.js
@@ -0,0 +1,14 @@
+const button =document.querySelector('button');
+
+button.addEventListener('click',()=>{
+
+const input1 = document.getElementById('first');
+const number1 = Number( input1.value);
+
+const input2 = document.getElementById('second');
+const number2 = Number( input2.value);
+
+const result = number1+number2;
+const re = document.getElementById('result');
+re.textContent = "Result: "+result;
+});
