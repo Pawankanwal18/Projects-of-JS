d0144531480a104441b9832507f7bb8ea678eaf7
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-29 18:49:05 +0530

    code is corected

--- project3.js
+++ project3.js
@@ -55,7 +55,7 @@ document.addEventListener("click", (e) => {
     // Stay on screen while growing
     setTimeout(() => {
         circle.classList.add("hide");
-    }, 3000);
+    }, 5000);
 
     // Remove after fade-out
     setTimeout(() => {
