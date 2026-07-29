dc2617c49eb546b8cc3efd053bcdb65424b0265d
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-29 11:52:25 +0530

    now inhance the proejct so that is work more smootly nad fast

--- project2.js
+++ project2.js
@@ -8,6 +8,9 @@ const number1 = Number( input1.value);
 const input2 = document.getElementById('second');
 const number2 = Number( input2.value);
 
+if(isNaN(number1)||isNaN(number2))
+    return;
+
 const result = number1+number2;
 const re = document.getElementById('result');
 re.textContent = "Result: "+result;
