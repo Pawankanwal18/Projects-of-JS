f3974ff4e42dc6b6e17a1cffa85d6a6ea94421f6
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-30 18:23:59 +0530

    js code is optimized and now this code is completly done

--- project4.js
+++ project4.js
@@ -29,3 +29,15 @@
 //     body.style.backgroundColor ='purple';
 
 // })
+
+ const buttons = document.querySelectorAll('button');
+ const body=document.body;
+
+ buttons.forEach((button)=>{
+
+   button.addEventListener('click',()=>{
+    
+    body.style.backgroundColor = button.id;
+
+   })
+ })
