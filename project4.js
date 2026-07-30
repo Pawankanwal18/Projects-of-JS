5081b30ae04bc244b60e1baab31553f5b1d74610
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-30 18:14:46 +0530

    now optimizing the code of java script

--- project4.js
+++ project4.js
@@ -1,31 +1,31 @@
-const red = document.getElementById('red');
-const blue = document.getElementById('blue');
-const orange = document.getElementById('orange');
-const green= document.getElementById('green');
-const purple = document.getElementById('purple');
-const body = document.body;
+// const red = document.getElementById('red');
+// const blue = document.getElementById('blue');
+// const orange = document.getElementById('orange');
+// const green= document.getElementById('green');
+// const purple = document.getElementById('purple');
+// const body = document.body;
 
-red.addEventListener('click',()=>{
-    body.style.backgroundColor ='red';
+// red.addEventListener('click',()=>{
+//     body.style.backgroundColor ='red';
 
-})
+// })
 
-blue.addEventListener('click',()=>{
-    body.style.backgroundColor ='blue';
+// blue.addEventListener('click',()=>{
+//     body.style.backgroundColor ='blue';
 
-})
+// })
 
-orange.addEventListener('click',()=>{
-    body.style.backgroundColor ='orange';
+// orange.addEventListener('click',()=>{
+//     body.style.backgroundColor ='orange';
 
-})
+// })
 
-green.addEventListener('click',()=>{
-    body.style.backgroundColor ='green';
+// green.addEventListener('click',()=>{
+//     body.style.backgroundColor ='green';
 
-})
+// })
 
-purple.addEventListener('click',()=>{
-    body.style.backgroundColor ='purple';
+// purple.addEventListener('click',()=>{
+//     body.style.backgroundColor ='purple';
 
-})
+// })
